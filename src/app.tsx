8d4855eb56d41124ba74eacaf4087e5c268e2d0f
Victor Araujo <victorwaraujo@gmail.com>
2025-04-07 17:12:01 -0300

    fixing problems

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,4 @@
 import { ChangeEvent, useState } from 'react'
-import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
